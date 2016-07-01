9608fcbe815e079f54f1497347b36318c7a92676
Oded Dwek <oded.walla@gmail.com>
2016-07-01 20:19:20 +0300

    Created the mock data.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('config');
+var jsonfile = require('jsonfile');
 
 // Set up the app
 var app = express();
@@ -16,13 +17,35 @@ app.use('/app', express.static('app'));
 var books = require('./controllers/book');
 app.use('/book', books);
 
+var Comment = require('./models/comment');
+var Book = require('./models/Book');
+
 //connect to our database
 var dbConfig = config.get('RottenAvocados.dbConfig');
 var connectionString = 'mongodb://' + dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.dbName;
-mongoose.connect(connectionString, function (err) {
-// Start the app
-    app.listen(8080, function () {
+mongoose.connect(connectionString, function(err, db) {
+
+    if (err != null) {
+        throw "Error connecting to mongo database: " + JSON.stringify(err);
+    }
+
+    Comment.remove({}, function(err) {});
+    Book.remove({}, function(err) {});
+    // Stores.remove({}, function(err) {});
+
+    // read data file
+    var file = jsonfile.readFile("Data/books.json", function(err, data) {
+
+        Book.insertMany(data.books, function(err, result) {
+            if (err)
+                console.log(err);
+        })
+    });
+
+
+
+    // Start the app
+    app.listen(8080, function() {
         console.log('Rotten Avocados listening on port 8080!');
     });
 });
-

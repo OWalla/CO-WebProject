d57e503b4ee5cf279ef5e27a7bcbdd0b5cd8601f
almogsad <almog_sad@walla.com>
2016-07-02 12:54:05 +0300

    Added the weather service

--- public/js/weatherService.js
+++ public/js/weatherService.js
@@ -1,15 +1,28 @@
+// weatherService.js
 
 var weatherCallback = function (data) {
-    var wind = data.query.results.channel.wind;
-    var item = data.query.results.channel.item;
-    $("#temperatureDiv h4").text("Weather: " + item.title);
-    var text = "Wind - chill: " + wind.chill + ", speed: " + wind.speed +
-        "</br> Temperature: " + item.condition.temp + " °C";
+    var wind = data.wind;
+    var main = data.main;
+    $("#temperatureDiv h4").text("Weather: " + data.name);
+    var text = "Wind: degree - " + wind.deg + ", speed - " + wind.speed +
+        "</br> Temperature: " + main.temp + " °C" +
+        "</br> Humidity percentage: " + main.humidity + "%";
     $("#temperatureDiv p").html(text);
 
-    if (item.condition.temp > 20 && item.condition.temp < 25) {
+    if (main.temp > 20 && main.temp < 25) {
         $("#temperatureDiv").append("<p><b>Recomendation: </b> Go out and don't read books.</p>");
     } else {
         $("#temperatureDiv").append("<p><b>Recomendation: </b> Stay at home and read books.</p>");
     }
 };
+
+var weatherFail = function () {
+    $("#temperatureDiv h4").text("Weather: " + data.name);
+    var text = "An error occurred while getting the weather.";
+    $("#temperatureDiv p").html(text);
+};
+
+function getWeather() {
+    var json = $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=294640&units=metric&appid=fce06179b578eb590b8fbf079c974951");
+    json.then(weatherCallback, weatherFail);
+}
